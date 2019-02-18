329c9d4bf9f4d1ad70a31c609c3602e61a9ca6c6
PedroFicuciello <phficuciello@gmail.com>
2019-02-18 19:52:07 -0300

    Declarar variavel, concatenar frases

---
+++ src/aulaJava/AulaJava.java
@@ -0,0 +1,21 @@
+package aulaJava;
+
+public class AulaJava {
+
+	public static void main(String[] args) {
+		//3
+		String a = "Curso de Java";
+		
+		//4 
+	a += (" - Exemplo String");
+		
+		
+		
+		
+		
+		System.out.println (a);
+		System.out.println (a);
+
+	}
+
+}
